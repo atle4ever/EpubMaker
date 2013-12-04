35fcb1b15bf20bb49deb07f0c706332e3f91c6f0
Seongjun Kim <atle4ever@gmail.com>
2013-12-05 02:46:36 +0900

    feed generation

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ FeedMaker
     $ sudo apt-get install libxslt-dev libxml2-dev python-lxml
 
 3. Install sqlite3
-    sudo apt-get install sqlite3
+    $ sudo apt-get install sqlite3
+
+4. Install webhelpers
+    $ sudo easy_install webhelpers
--- collect.py
+++ collect.py
@@ -2,10 +2,44 @@ import urllib2
 import sqlite3 as lite
 import sys
 from lxml import etree
+from webhelpers.feedgenerator import Atom1Feed
+
+RUN_DIR = '/home/sjkim/FeedMaker/'
+DROPBOX_PUBIC = '/home/sjkim/Dropbox/public/'
+
+def publish(cur, site, url_id, url_subject):
+    if site == 'munpia':
+        url = "http://novel.munpia.com/{0}".format(url_id)
+    elif site == 'naver':
+        url = "http://novel.naver.com/webnovel/list.nhn?novelId={0}".format(url_id)
+    else:
+        print "Error: invliad site {0}".format(site)
+        sys.exit(-1)
+
+    feed = Atom1Feed(
+            title=url_subject,
+            link=url,
+            description=url_subject,
+            language=u"kr",
+            )
+
+
+    cur.execute("SELECT subject, link FROM article WHERE site = ? AND url_id = ? order by id desc", (site, url_id))
+    ret = cur.fetchall()
+    for a in ret:
+        article_subject = a[0]
+        article_link = a[1]
+        feed.add_item(title=article_subject,
+                link=article_link,
+                description="")
+
+    xml = open(DROPBOX_PUBIC + "{0}_{1}.xml".format(site, url_id), "w")
+    xml.write(feed.writeString('utf-8'))
+    xml.close()
 
 def crawl():
     # init sqlite3
-    con = lite.connect('feed.db')
+    con = lite.connect(RUN_DIR + 'feed.db')
     cur = con.cursor()
     
     # get url_id and subject
@@ -19,14 +53,19 @@ def crawl():
         
         if site == 'munpia':
             url = "http://novel.munpia.com/{0}".format(url_id)
-            crawlMunpia(cur, url, url_id, url_subject);
+            hasNew = crawlMunpia(cur, url, url_id, url_subject)
+
         elif site == 'naver':
             url = "http://novel.naver.com/webnovel/list.nhn?novelId={0}".format(url_id)
-            crawlNaver(cur, url, url_id, url_subject);
+            hasNew = crawlNaver(cur, url, url_id, url_subject);
+
         else:
             print "Error: invliad site {0}".format(site)
             sys.exit(-1)
         con.commit()
+
+        if hasNew == True:
+            publish(cur, site, url_id, url_subject)
 
     con.close()
     
@@ -40,6 +79,7 @@ def crawlNaver(cur, url, url_id, url_subject):
     hparser = etree.HTMLParser(encoding='utf-8')
     doc = etree.fromstring(doc, hparser)
     articles = doc.xpath("//*[@id=\"content\"]/div/div[1]/table/tbody/tr")
+    hasNew = False
     for a in articles:
         article_link = 'http://novel.naver.com/' + a.xpath("./td[1]/a/@href")[0]
         article_subject = a.xpath("./td[1]/a/text()")[0]
@@ -54,6 +94,9 @@ def crawlNaver(cur, url, url_id, url_subject):
             cur.execute("INSERT INTO article(site, url_id, id, subject, link) VALUES (?, ?, ?, ?, ?)",
                     ('naver', url_id, article_id, article_subject, article_link))
             print "New article {0} {1} {2}".format(url_id, article_id, article_link)
+            hasNew = True
+
+    return hasNew
 
 def crawlMunpia(cur, url, url_id, url_subject):
     # get html
@@ -65,6 +108,7 @@ def crawlMunpia(cur, url, url_id, url_subject):
     hparser = etree.HTMLParser(encoding='utf-8')
     doc = etree.fromstring(doc, hparser)
     articles = doc.xpath("//*[@id=\"ENTRIES\"]/tbody/tr")
+    hasNew = False
     for a in articles:
         article_class = a.xpath("./@class")
         if len(article_class) > 0 and article_class[0] == "notice":
@@ -82,10 +126,13 @@ def crawlMunpia(cur, url, url_id, url_subject):
             cur.execute("INSERT INTO article(site, url_id, id, subject, link) VALUES (?, ?, ?, ?, ?)",
                     ('munpia', url_id, article_id, article_subject, article_link))
             print "New article {0} {1} {2}".format(url_id, article_id, article_link)
+            hasNew = True
+    
+    return hasNew
 
 def newUrlNaver(site, url_id):
     # init sqlite3
-    con = lite.connect('feed.db')
+    con = lite.connect(RUN_DIR + 'feed.db')
     cur = con.cursor()
     
     # get url_id and subject
@@ -112,7 +159,7 @@ def newUrlNaver(site, url_id):
 
 def newUrlMunpia(site, url_id):
     # init sqlite3
-    con = lite.connect('feed.db')
+    con = lite.connect(RUN_DIR + 'feed.db')
     cur = con.cursor()
     
     # get url_id and subject
