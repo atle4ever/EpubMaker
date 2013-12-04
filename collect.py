6158bad194b7478119da8a950c367e21faa62db3
Seongjun Kim <atle4ever@gmail.com>
2013-12-05 00:48:44 +0900

    enable novel.naver.com

--- collect.py
+++ collect.py
@@ -9,52 +9,114 @@ def crawl():
     cur = con.cursor()
     
     # get url_id and subject
-    cur.execute("SELECT id, subject FROM url")
+    cur.execute("SELECT site, id, subject FROM url")
     urls = cur.fetchall()
     
     for url in urls:
-        url_id = url[0]
-        url = "http://novel.munpia.com/{0}".format(url_id)
-        url_subject = url[1]
-    
-        # get html
-        usock = urllib2.urlopen(url)
-        doc = usock.read()
-        usock.close()
+        site = url[0]
+        url_id = url[1]
+        url_subject = url[2]
         
-        # html parsing
-        hparser = etree.HTMLParser(encoding='utf-8')
-        doc = etree.fromstring(doc, hparser)
-        articles = doc.xpath("//*[@id=\"ENTRIES\"]/tbody/tr")
-        for a in articles:
-            article_class = a.xpath("./@class")
-            if len(article_class) > 0 and article_class[0] == "notice":
-                continue
-            article_id = a.xpath("./td[1]/span/text()")[0]
-            article_subject = a.xpath("./td[2]/a[1]/text()")[0]
-            article_link = 'http://novel.munpia.com' + a.xpath("./td[2]/a[1]/@href")[0]
-            article_date = a.xpath("./td[3]/text()")[0]
-        
-            cur.execute("SELECT count(*) FROM article WHERE url_id = ? AND id = ?", (url_id, article_id))
-            ret = cur.fetchall()
-    
-            # new article
-            if(ret[0][0] == 0): 
-                cur.execute("INSERT INTO article VALUES (?, ?, ?, ?)",
-                        (url_id, article_id, article_subject, article_link))
-                print "New article {0} {1} {2}".format(url_id, article_id, article_link)
-        
+        if site == 'munpia':
+            url = "http://novel.munpia.com/{0}".format(url_id)
+            crawlMunpia(cur, url, url_id, url_subject);
+        elif site == 'naver':
+            url = "http://novel.naver.com/webnovel/list.nhn?novelId={0}".format(url_id)
+            crawlNaver(cur, url, url_id, url_subject);
+        else:
+            print "Error: invliad site {0}".format(site)
+            sys.exit(-1)
         con.commit()
-    
-    con.close()
 
-def newUrl(url_id):
+    con.close()
+    
+def crawlNaver(cur, url, url_id, url_subject):
+    # get html
+    usock = urllib2.urlopen(url)
+    doc = usock.read()
+    usock.close()
+
+    # html parsing
+    hparser = etree.HTMLParser(encoding='utf-8')
+    doc = etree.fromstring(doc, hparser)
+    articles = doc.xpath("//*[@id=\"content\"]/div/div[1]/table/tbody/tr")
+    for a in articles:
+        article_link = 'http://novel.naver.com/' + a.xpath("./td[1]/a/@href")[0]
+        article_subject = a.xpath("./td[1]/a/text()")[0]
+        article_id = article_link[article_link.rfind('volumeNo=') + 9:];
+        article_date = a.xpath("./td[2]/text()")[0]
+
+        cur.execute("SELECT count(*) FROM article WHERE site = ? AND url_id = ? AND id = ?", ('naver', url_id, article_id))
+        ret = cur.fetchall()
+
+        # new article
+        if(ret[0][0] == 0): 
+            cur.execute("INSERT INTO article(site, url_id, id, subject, link) VALUES (?, ?, ?, ?, ?)",
+                    ('naver', url_id, article_id, article_subject, article_link))
+            print "New article {0} {1} {2}".format(url_id, article_id, article_link)
+
+def crawlMunpia(cur, url, url_id, url_subject):
+    # get html
+    usock = urllib2.urlopen(url)
+    doc = usock.read()
+    usock.close()
+    
+    # html parsing
+    hparser = etree.HTMLParser(encoding='utf-8')
+    doc = etree.fromstring(doc, hparser)
+    articles = doc.xpath("//*[@id=\"ENTRIES\"]/tbody/tr")
+    for a in articles:
+        article_class = a.xpath("./@class")
+        if len(article_class) > 0 and article_class[0] == "notice":
+            continue
+        article_id = a.xpath("./td[1]/span/text()")[0]
+        article_subject = a.xpath("./td[2]/a[1]/text()")[0]
+        article_link = 'http://novel.munpia.com' + a.xpath("./td[2]/a[1]/@href")[0]
+        article_date = a.xpath("./td[3]/text()")[0]
+    
+        cur.execute("SELECT count(*) FROM article WHERE site = ? AND url_id = ? AND id = ?", ('munpia', url_id, article_id))
+        ret = cur.fetchall()
+
+        # new article
+        if(ret[0][0] == 0): 
+            cur.execute("INSERT INTO article(site, url_id, id, subject, link) VALUES (?, ?, ?, ?, ?)",
+                    ('munpia', url_id, article_id, article_subject, article_link))
+            print "New article {0} {1} {2}".format(url_id, article_id, article_link)
+
+def newUrlNaver(site, url_id):
     # init sqlite3
     con = lite.connect('feed.db')
     cur = con.cursor()
     
     # get url_id and subject
-    cur.execute("SELECT count(*) FROM url WHERE id = ?", (url_id,))
+    cur.execute("SELECT count(*) FROM url WHERE site = ? AND id = ?", (site, url_id))
+    ret = cur.fetchone()
+    if ret[0] != 0:
+        print "Existing url id"
+        return
+
+    # get subject
+    url = "http://novel.naver.com/webnovel/list.nhn?novelId={0}".format(url_id)
+    usock = urllib2.urlopen(url)
+    doc = usock.read()
+    usock.close()
+        
+    hparser = etree.HTMLParser(encoding='utf-8')
+    doc = etree.fromstring(doc, hparser)
+    subject = doc.xpath("//*[@id=\"content\"]/div/div[1]/div[1]/div/h2/text()")[0].strip()
+
+    cur.execute("INSERT INTO url(site, id, subject) VALUES (?, ?, ?)", (site, url_id, subject))
+    print "New url id: {0}".format(url_id)
+    con.commit()
+    con.close()
+
+def newUrlMunpia(site, url_id):
+    # init sqlite3
+    con = lite.connect('feed.db')
+    cur = con.cursor()
+    
+    # get url_id and subject
+    cur.execute("SELECT count(*) FROM url WHERE site = ? AND id = ?", (site, url_id))
     ret = cur.fetchone()
     if ret[0] != 0:
         print "Existing url id"
@@ -70,14 +132,14 @@ def newUrl(url_id):
     doc = etree.fromstring(doc, hparser)
     subject = doc.xpath("//*[@id=\"board\"]/div[1]/div[2]/h2/a/text()")[0]
 
-    cur.execute("INSERT INTO url VALUES (?, ?)", (url_id, subject))
+    cur.execute("INSERT INTO url(site, id, subject) VALUES (?, ?, ?)", (site, url_id, subject))
     print "New url id: {0}".format(url_id)
     con.commit()
     con.close()
 
 def usage():
     print "usage 1: python {0} crawl".format(sys.argv[0])
-    print "usage 2: python {0} add <url id>".format(sys.argv[0])
+    print "usage 2: python {0} add <site> <url id>".format(sys.argv[0])
     return
 
 if len(sys.argv) == 1:
@@ -86,10 +148,18 @@ if len(sys.argv) == 1:
 
 if sys.argv[1] == 'crawl':
     crawl()
-else:
-    if len(sys.argv) < 3:
+else: # add command
+    if len(sys.argv) < 4:
         usage()
         sys.exit(-1)
 
-    url_id = int(sys.argv[2])
-    newUrl(url_id)
+    site = sys.argv[2]
+    url_id = int(sys.argv[3])
+
+    if site == 'munpia':
+        newUrlMunpia(site, url_id)
+    elif site == 'naver':
+        newUrlNaver(site, url_id)
+    else:
+        print "Invalid site {0}".format(site)
+        sys.exit(-1)
--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE url (id INTEGER primary key, subject VARCHAR(1024));
-CREATE TABLE article (url_id INTEGER, id INTEGER, subject VARCHAR(1024), link VARCHAR(1024), primary key (url_id, id));
+CREATE TABLE url (site VARCHAR(20), id INTEGER, subject VARCHAR(1024), primary key(site, id));
+CREATE TABLE article (site VARCHAR(20), url_id INTEGER, id INTEGER, subject VARCHAR(1024), link VARCHAR(1024), primary key (site, url_id, id));
